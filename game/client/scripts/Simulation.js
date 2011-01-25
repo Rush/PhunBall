b98d6d1a700999493fd10d8f3027361ef9f23220
Ravadre <ravadrespam@gmail.com>
2011-01-25 18:21:47 +0100

    fixed simulation time step

--- game/client/scripts/Simulation.js
+++ game/client/scripts/Simulation.js
@@ -3,33 +3,53 @@
 
 function Simulation(field)
 {
-	var friction = 0.95;
-	var force = 1000.0;
+    var friction = 0.95;
+    var force = 1000.0;
 
-	function updatePlayer(player, time)
-	{
-		player.position.x += player.velocity.x * time;
-		player.position.y += player.velocity.y * time;
-		player.position.clamp(0, field.width, 0, field.height);
-		player.velocity.mul(friction);
-	}
+    var simulationStep = 0.033;
+    var simulationTimeAgg = 0.0;
 
-	this.update = function (time)
-	{
-		updatePlayer(field.player, time);
+    function updatePlayer(player, time)
+    {
+        player.position.x += player.velocity.x * time;
+        player.position.y += player.velocity.y * time;
+        player.position.clamp(0, field.width, 0, field.height);
+        player.velocity.mul(friction);
+    }
 
-		// TODO: update ball
-		// TODO: update other players
-		// TODO: fixed time simulation steps
-	};
+    this.update = function (time)
+    {
+        simulationTimeAgg += time;
 
-	this.applyForce = function (vector, time)
-	{
-		vector.normalize();
-		vector.mul(force * time);
+     
+        while (simulationTimeAgg > simulationStep)
+        {
+            var step = simulationStep;
+            
+            //TODO: Log this somewhere
+            //TODO: Think about repairing this differently
+            //      Situation can be fixed by flagging client state as corrupted and pulling whole state from server
+            //      For server it ain't that easy
+            if (simulationTimeAgg > simulationStep * 10)
+            {
+                step = simulationStep * 10;
+            }
 
-		field.player.velocity.add(vector);
-	};
+            updatePlayer(field.player, step);
+            simulationTimeAgg -= step;
+        }
+
+        // TODO: update ball
+        // TODO: update other players
+    };
+
+    this.applyForce = function (vector, time)
+    {
+        vector.normalize();
+        vector.mul(force * time);
+
+        field.player.velocity.add(vector);
+    };
 }
 
 module.exports = Simulation;
